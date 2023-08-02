fadea9cc42cdcd50c37a511a6b8fea569abb28f0
gi8 <giottolino@gmail.com>
2023-08-02 15:57:40 +0200

    simplifiy code

--- certalert.py
+++ certalert.py
@@ -32,13 +32,14 @@ class CertificateInfo:
         path (str): The file path to the certificate.
         enabled (Optional[bool], optional): Indicates whether the certificate is enabled or disabled.
                                             Defaults to True if not specified.
-        type Optional[str]: The type of the certificate. Possible values are: 'pem', 'pkcs12' or 'jks'.
+        type Optional[str]: The type of the certificate. Possible values are: 'crt', 'pem', 'pkcs12' or 'jks'.
                             If not specified, the type is guessed based on the file extension.
         password (Optional[str], optional): The password to access the certificate
                                             if it is password-protected. Defaults to None.
-        alias (Optional[str], optional): Represents either the certificate's alias in the keystore
-                                         or the name of the desired certificate within a psk12 file
-                                         containing multiple certificates.
+        position (Optional[int], optional): The position of the certificate in a PEM or PKCS12 file.
+                                            If not set and the file contains multiple certificates,
+                                            the first certificate in the chain is used.
+        alias (Optional[str], optional): The alias of the certificate in the JKS file.
                                          Defaults to None.
         labels (Optional[Dict[str, str]], optional): Additional labels to be added to the metric.
     """
@@ -47,6 +48,7 @@ class CertificateInfo:
     enabled: Optional[bool] = True
     type: Optional[str] = None
     password: Optional[str] = None
+    position: Optional[int] = None
     alias: Optional[str] = None
     labels: Optional[Dict[str, str]] = None
 
@@ -67,7 +69,14 @@ def parse_args() -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(
         description="certalert - Monitor SSL/TLS certificates and push alerts to Prometheus Pushgateway",
-        epilog="",)
+        epilog="""
+certalert can extract the expiration date from the following certificate types:
+- PEM (crt, pem)
+- PKCS12 (p12)
+- JKS (jks)
+
+The certificate type can be specified in the config file or it can be guessed based on the file extension.
+""",)
     parser.add_argument('--config',
                         '-c',
                         type=str,
@@ -172,11 +181,7 @@ def check_config(config: dict) -> None:
         try:
             cert = CertificateInfo(**cert)
         except Exception as e:
-            msg = str(e).lstrip("CertificateInfo.__init__() ")  # remove ugly prefix
-            msg = msg[:1].upper() + msg[1:]  # capitalize first letter
-            if not (cert_name := cert.get('name')):
-                raise TypeError(f"Invalid certificate no {idx}. {msg}")
-            raise TypeError(f"Invalid certificate '{cert_name}'. {msg}")
+            raise TypeError(f"{str(e).removeprefix('CertificateInfo.__init__() ').capitalize()}")
 
         try:
             if not cert.type:
@@ -190,21 +195,37 @@ def check_config(config: dict) -> None:
                 raise AttributeError("Key 'path' is not specified.")
 
             if not os.path.isfile(cert.path):
-                raise FileNotFoundError(f"Certificate '{cert.name}' ({cert.path}) not found.")
+                raise FileNotFoundError(f"Certificate '{cert.path}' not found.")
 
             if (password := cert.password):
                 cert.password = resolve_variable(password)
 
-            certs[idx] = cert # replace the dict with the CertificateInfo object
+            if cert.position is not None:
+                if cert.position < 1:
+                    raise ValueError("Key 'position' must be greater one.")
+
+                try:
+                    cert.position = int(cert.position - 1)
+                except Exception:
+                    raise TypeError("Key 'position' must be an integer.")
+
+            if cert.alias:
+                cert.alias = str(cert.alias)  # convert to string in case it is an integer
+
+            certs[idx] = cert  # replace the dict with the CertificateInfo object
 
         except Exception as e:
-            raise Exception(f"Invalid certificate '{cert.name}' ({cert.path}). {str(e)}")
+            if not isinstance(cert, CertificateInfo) and not cert.get('name'):
+                raise Exception(f"Invalid certificate no {idx}. {str(e)}")
+            raise Exception(f"Invalid certificate '{cert.name}'. {str(e)}")
 
 
 def guess_certificate_type(cert_path: str) -> str:
     """Guess the type of a certificate based on its file extension."""
 
-    if cert_path.endswith('.pem'):
+    if cert_path.endswith('.crt'):
+        return 'crt'
+    elif cert_path.endswith('.pem'):
         return 'pem'
     elif cert_path.endswith('.p12'):
         return 'pkcs12'
@@ -230,49 +251,41 @@ def extract_expiration_pem(cert: CertificateInfo) -> int:
         logging.debug(f"Password for certificate '{cert.name}' ({cert.path}) is valid.")
 
     # Load the PEM data into an X.509 certificate object
-    cert = x509.load_pem_x509_certificate(data=pem_data,
-                                          backend=default_backend())
+    certs = x509.load_pem_x509_certificates(data=pem_data)
 
-    epoch_time = int((cert.not_valid_after - datetime.datetime(1970, 1, 1)).total_seconds())
+    if cert.position and len(certs) < cert.position:
+        raise ValueError(f"Certificate position '{cert.position}' is out of range.")
+
+    cert = certs[cert.position or 0]  # if position is not set, use the first certificate in the chain
+
+    epoch_time = int(cert.not_valid_after.timestamp())
     return epoch_time
 
 
 def extract_expiration_p12(cert: CertificateInfo) -> int:
     """Extract the expiration date of a certificate from a P12 file and return it as a Linux epoch."""
 
-    def find_matching_certificate(p12_certificates, alias):
-        """Find a certificate in a P12 file by its alias.
-        """
-        for c in p12_certificates[1:]:
-            if isinstance(c, x509.Certificate) and c.issuer.rfc4514_string() == alias:
-                return c
-        return None
-
     with open(cert.path, 'rb') as p12_file:
         p12_data = p12_file.read()
 
+    # An empty password is technically allowed in PKCS12 files
+    p12_password = cert.password.encode() if cert.password is not None else None
+
     # Load the P12 data into a KeyStore object using the provided password
-    p12 = pkcs12.load_key_and_certificates(data=p12_data,
-                                           password=cert.password.encode() if cert.password else None,
-                                           backend=default_backend())
+    _, cert, additional_certs = pkcs12.load_key_and_certificates(
+        data=p12_data,
+        password=p12_password,
+        backend=default_backend()
+    )
 
-    # Validate the number of certificates in the P12 file
-    num_certificates = len(p12)
-    if num_certificates < 2:
-        raise ValueError("No certificate found in the P12 file.")
+    certs = [cert] + additional_certs
 
-    if num_certificates > 3 and not cert.alias:
-        raise KeyError("There are more than one certificate but no alias was set.")
+    if cert.position and len(certs) < cert.position:
+        raise ValueError(f"Certificate position '{cert.position}' is out of range.")
 
-    if num_certificates > 3:
-        logging.debug("More than one certificate found in the P12 file. Using alias as filter.")
-        if (matching_certificate := find_matching_certificate(p12, cert.alias)) is None:
-            raise ValueError(f"Alias '{cert.alias}' not found in the P12 file.")
-        expiration_date = matching_certificate.not_valid_after
-    else:
-        expiration_date = p12[1].not_valid_after  # The second element is always the certificate
+    cert = certs[cert.position or 0]  # if position is not set, use the first certificate in the chain
 
-    epoch_time = int((expiration_date - datetime.datetime(1970, 1, 1)).total_seconds())
+    epoch_time = int(cert.not_valid_after.timestamp())
     return epoch_time
 
 
@@ -290,20 +303,14 @@ def extract_expiration_jks(cert: CertificateInfo) -> int:
     if not (cert_entry := keystore.entries.get(cert.alias)):
         raise ValueError(f"Alias '{cert.alias}' not found in the JKS file.")
 
-    # iterate over certhaings in the keystore
-    for c in cert_entry.cert_chain:
-        logging.debug(f"Found certificate with alias '{cert.alias}' in the JKS file.")
-        # load x509 certificate into cryptography object
-        cert_x509 = x509.load_der_x509_certificate(data=c[2],
-                                                   backend=default_backend())
-        # print the expiration date
-        logging.debug(f"Certificate '{cert.alias}' expires on {cert_x509.not_valid_after}")
+    last_cert_in_chain = cert_entry.cert_chain[0]  # get the first certificate in the chain
+    logging.debug(f"Found certificate with alias '{cert.alias}' in the JKS file.")
+    # load x509 certificate into cryptography object
+    cert_x509 = x509.load_der_x509_certificate(data=last_cert_in_chain[1],  # get the certificate data
+                                               backend=default_backend())
 
     # Retrieve the certificate entry and extract the expiration date
-    expiration_date = datetime.datetime.utcfromtimestamp(cert_entry.cert.get_not_after() / 1000)
-
-    # Convert the expiration date to a Linux epoch and return it
-    epoch_time = int((expiration_date - datetime.datetime(1970, 1, 1)).total_seconds())
+    epoch_time = int(cert_x509.not_valid_after.timestamp())
     return epoch_time
 
 
@@ -311,6 +318,7 @@ def extract_expiration_jks(cert: CertificateInfo) -> int:
 # functions for extracting the expiration date of certificates.
 CERTIFICATE_TYPES = {
     'pem': extract_expiration_pem,
+    'crt': extract_expiration_pem,
     'pkcs12': extract_expiration_p12,
     'jks': extract_expiration_jks
 }
@@ -520,8 +528,7 @@ def main():
         sys.exit(1)
 
     # Setup logging
-    level = logging.DEBUG if args.verbose else os.environ.get('LOG_LEVEL', logging.INFO)
-    setup_logger(level)
+    setup_logger(logging.DEBUG if args.verbose else logging.INFO)
 
     # Read and parse the configuration file
     try:
